0b177921dab68f110ce1b24e0edadf3cc5720ed1
jpsierens <jpsierens1988@gmail.com>
2016-08-17 01:21:23 +0200

    check if path is dir or file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
 const effjay = (rootDirectory) => {
+	let root = fs.readdirSync(rootDirectory);
+	console.log(root);
 
+	let file1 = fs.statSync(`${rootDirectory}/${root[0]}`);
+	console.log(file1.isFile());
 }
+
+effjay('test');
