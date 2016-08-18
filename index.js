232129db635d776cdc9a905580e4b76679ceb136
jpsierens <jpsierens1988@gmail.com>
2016-08-18 12:01:15 +0200

    validations + generator

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 
+const generateJSON = (path, json) => {
+
+	path.forEach(subPath => {
+		console.log(subPath);
+	});
+};
+
 const effjay = (rootDirectory) => {
-	let root = fs.readdirSync(rootDirectory);
+	const root = fs.readdirSync(rootDirectory);
 	console.log(root);
 
-	let file1 = fs.statSync(`${rootDirectory}/${root[0]}`);
-	console.log(file1.isFile());
+	if (!root) throw 'Error: A root directory is required';
+	if (fs.statSync(rootDirectory).isFile()) throw 'Error: You passed in a file path, you need to pass in a directory path';
+
+	return generateJSON(root, {});
 }
 
 effjay('test');
