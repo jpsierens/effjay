54bcea10cceafe3389191b9630d14ebfe7eb58a6
jpsierens <jpsierens1988@gmail.com>
2016-08-18 12:10:52 +0200

    dir, not path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const generateJSON = (path, json) => {
+const generateJSON = (dir, json) => {
 
-	path.forEach(subPath => {
+	dir.forEach(subPath => {
 		console.log(subPath);
 	});
 };
@@ -11,8 +11,13 @@ const effjay = (rootDirectory) => {
 	const root = fs.readdirSync(rootDirectory);
 	console.log(root);
 
-	if (!root) throw 'Error: A root directory is required';
-	if (fs.statSync(rootDirectory).isFile()) throw 'Error: You passed in a file path, you need to pass in a directory path';
+	if (!root) {
+		throw 'Error: A root directory is required';
+	}
+	if (fs.statSync(rootDirectory).isFile()) {
+		throw 'Error: You passed in a file path, '+
+			'you need to pass in a directory path';
+	}
 
 	return generateJSON(root, {});
 }
